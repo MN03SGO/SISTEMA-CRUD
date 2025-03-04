875cbfb95b754b18bddce10be280ea4c0846bc4d
Anthony Manuel Sigarán <146805872+MN03SGO@users.noreply.github.com>
2025-03-04 14:50:18 -0600

    DASH
    comienzo

--- CRUD (SQL, PY)/interfaces/index.py
+++ CRUD (SQL, PY)/interfaces/index.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter  as tk 
 import mysql.connector
 
 ###ARREGLO DE VENTANA
@@ -7,14 +8,25 @@ ventana= Tk()
 ventana.title("CRUD")
 ventana.geometry("1920x1980")
 
+def barra_menu(ventana):
+    barra_menu = tk.Menu(ventana)
+    ventana.config(menu=barra_menu, width= 300, heigth=300)
+
+    barra_menu = tk.Menu(barra_menu)
+    barra_menu.add_cascade(Label= "Inicio", menu=barra_menu)
+
+    barra_menu.add_command(Label= "Crear registro en BD")
+    barra_menu.add_command(Label= "Eliminar registro en BD")
+    barra_menu.add_command(Label= "Salir")
 
 
 
 
-imagenUsu = PhotoImage(file = "img/USUARIO1.png").grid(sticky=W)
-label_imgUsu = Label(ventana, image=imagenUsu)
-label_imgUsu.place(x=1202, y=340)
 
-ventana.imagenUsu = imagenUsu
+#imagenUsu = PhotoImage(file = "img/USUARIO1.png").grid(sticky=W)
+#label_imgUsu = Label(ventana, image=imagenUsu)
+#label_imgUsu.place(x=1202, y=340)
+
+#ventana.imagenUsu = imagenUsu
 
 mainloop()
